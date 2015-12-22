6cd30f40adb187aa30f8032b4df9b168dff580d9
Matthew Turner <matthew.turner@nib.com.au>
2015-12-23 08:57:54 +1100

    Fix spacing

--- src/index.js
+++ src/index.js
@@ -9,23 +9,23 @@ module.exports = function(rootPath){
 	process.chdir(dirToChangeTo);
 
 	process.env['NO_PAUSE'] = true;
-	
+
   var args = process.argv.slice(2);
-	
+
   for(var i = 0; i < args.length; i++) {
     args[i] = args[i].replace('@root', rootPath);
   }
-  
-	var child = spawn('gatling.bat', args);
-	
+
+  var child = spawn('gatling.bat', args);
+
   process.on('exit', function() {
     child.kill();
   });
-  
-	child.stdout.on('data', function(chunk){
-		console.log(chunk.toString()); 
-	});
-	
+
+  child.stdout.on('data', function(chunk){
+    console.log(chunk.toString()); 
+  });
+
 	child.stderr.on('data', function(chunk){
 		console.log(chunk.toString()); 
 	});
