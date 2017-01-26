0217a031d791363c4d98744fb6ac388acca2a4ae
Matthew Turner <matthew.turner@nib.com.au>
2017-01-27 10:04:31 +1100

    Fix up indenting

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ var spawn = require('child_process').spawn;
 module.exports = function(rootPath){
 
   var dirToChangeTo = __dirname + '\\lib\\bin';
-	process.chdir(dirToChangeTo);
-
+	
+  process.chdir(dirToChangeTo);
 	process.env['NO_PAUSE'] = true;
-
+  
   var args = process.argv.slice(2);
 
   for(var i = 0; i < args.length; i++) {
     args[i] = args[i].replace('~', rootPath);
   }
-
+  
   var child = spawn('gatling.bat', args);
 
   process.on('exit', function() {
@@ -26,7 +26,7 @@ module.exports = function(rootPath){
     console.log(chunk.toString()); 
   });
 
-	child.stderr.on('data', function(chunk){
-		console.log(chunk.toString()); 
-	});
+  child.stderr.on('data', function(chunk){
+    console.log(chunk.toString()); 
+  });
 }
